9fdfaba3f4cb986d7ead9e7bc6bab3020ef9fd5c
rakib13x <rakib13x@gmail.com>
2024-01-04 11:16:10 +0600

    Find Maximum Value of an array is added

---
+++ Js-Challange/Find_Maximum.js
@@ -0,0 +1,26 @@
+function findMaxElement(arr) {
+  if (arr.length === 0) {
+    console.log("Your Input Element is empty.");
+    return;
+  }
+
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "number") {
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+    } else {
+      console.log("Your input Element is not a number.");
+    }
+  }
+  return max;
+}
+
+console.time("findMaxElement");
+const result = findMaxElement([1, 3, 5, 6, 7, 89, 0, 6, 4, 3, 54, 5, 6]);
+console.timeEnd("findMaxElement");
+
+console.log(result);
+//time complexicity: O(n)
+//space complexicity: O(1)
