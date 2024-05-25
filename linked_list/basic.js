3082f870f1f9552223d60ea4b0c2359265eeb5b9
rakib13x <rakib13x@gmail.com>
2024-05-25 23:43:10 +0600

    Linked List with it's basic method added

--- linked_list/basic.js
+++ linked_list/basic.js
@@ -90,18 +90,78 @@ class LinkedList {
     return temp;
   }
 
-  set(index, value) {}
+  set(index, value) {
+    let temp = this.get(index);
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, value) {
+    if (index === 0) {
+      return this.unshift(value);
+    }
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+    if (index === this.length) {
+      return this.push(value);
+    }
+
+    const newNode = new Node(value);
+    const temp = this.get(index - 1);
+    newNode.next = temp.next;
+    temp.next = newNode;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index === 0) {
+      return this.shift();
+    }
+    if (index === this.length - 1) {
+      return this.pop();
+    }
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+    const before = this.get(index - 1);
+    const temp = before.next;
+
+    before.next = temp.next;
+    temp.next = null;
+    this.length--;
+    return temp;
+  }
+
+  reverse() {
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+    let next = temp.next;
+    let prev = null;
+
+    for (let i = 0; i < this.length; i++) {
+      next = temp.next;
+      temp.next = prev;
+      prev = temp;
+      temp = next;
+    }
+    return this;
+  }
 }
 
 let myLinkedList = new LinkedList(4);
 
-myLinkedList.shift();
-myLinkedList.shift();
-myLinkedList.shift();
-myLinkedList.shift();
-myLinkedList.shift();
-myLinkedList.shift();
-myLinkedList.shift();
-console.log(myLinkedList.get(-1));
-
+myLinkedList.push(4);
+myLinkedList.push(6);
+myLinkedList.push(7);
+myLinkedList.push(8);
+myLinkedList.push(9);
+myLinkedList.push(10);
+myLinkedList.remove(1);
+myLinkedList.reverse();
 console.log(myLinkedList);
