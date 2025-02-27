135899cad2d1a0fea100c061fabc5574f5960463
rakib13x <rakib13x@gmail.com>
2025-02-28 00:42:54 +0600

    insertion algorithm  added

---
+++ Algorithm/insertion-sort/basic.js
@@ -0,0 +1,13 @@
+function insertionSort(array) {
+  let temp;
+  for (let i = 1; i < array.length; i++) {
+    temp = array[i];
+    for (var j = i - 1; array[j] > temp && j > -1; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = temp;
+  }
+  return array;
+}
+
+console.log(insertionSort([2, 4, 3, 6, 7, 3]));
