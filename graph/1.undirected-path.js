2f9d8d2709e8d69d9243c7483e382cdb344eddaa
rakib13x <rakib13x@gmail.com>
2025-03-18 19:11:34 +0600

    shortest path and undirected path solution added

---
+++ graph/1.undirected-path.js
@@ -0,0 +1,44 @@
+const undirectedPath = (edges, nodeA, nodeB) => {
+  const graph = buildGraph(edges);
+  console.log(graph);
+  return hasPath(graph, nodeA, nodeB, new Set());
+};
+
+const hasPath = (graph, src, dst, visited) => {
+  if (src === dst) return true;
+  if (visited.has(src)) return false;
+
+  visited.add(src);
+
+  for (let neighbor of graph[src]) {
+    if (hasPath(graph, neighbor, dst, visited) === true) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+const buildGraph = (edges) => {
+  const graph = {};
+
+  for (let edge of edges) {
+    const [a, b] = edge;
+    if (!(a in graph)) graph[a] = [a];
+    if (!(b in graph)) graph[b] = [b];
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+
+  return graph;
+};
+
+const edges = [
+  ["i", "j"],
+  ["k", "i"],
+  ["m", "k"],
+  ["k", "l"],
+  ["o", "n"],
+];
+
+undirectedPath(edges, "j", "m");
